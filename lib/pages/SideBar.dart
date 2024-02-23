346ae66a1f32a82ce8b601a10301d8d887f1ae27
Siwar Chabbi <siwarchabbi7@gmail.com>
2024-02-23 19:57:55 +0100

    Add SidBar And show Offre of user And Udait Offre

---
+++ lib/pages/SideBar.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:frontendcovoituragemobile/pages/MyOffer.dart';
+import 'MyOffer.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class SideBar extends StatefulWidget {
+  @override
+  _SideBarState createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  late String _username = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserData();
+  }
+
+  Future<void> _fetchUserData() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString('token');
+      final userId = prefs.getString('userId');
+
+      if (userId == null) {
+        throw Exception('User ID is null');
+      }
+
+      final response = await http.get(
+        Uri.parse('http://localhost:5000/api/users/profile/$userId'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final userData = json.decode(response.body);
+        setState(() {
+          _username = userData['username'];
+        });
+      } else {
+        print('Failed to load user data. Status code: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error loading user data: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Color(0xFF009C77),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/images/car.png'),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  _username,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home Page'),
+            onTap: () {
+
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.list),
+            title: Text('My Offers'),
+            onTap: () {
+              Navigator.pop(context);
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MyOffersPage()),
+              );
+
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings'),
+            onTap: () {
+
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('About Us'),
+            onTap: () {
+
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
